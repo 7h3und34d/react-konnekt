4edbe915e36cacb9fa0a5caaf7f516735fd2442f
bzdv21 <boaz.dev.21+github@gmail.com>
2021-03-17 22:53:03 +0200

    Add konnekt options

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,35 @@
 import * as React from 'react';
+import hoistStatics from 'hoist-non-react-statics';
 
 interface HooksToPropsFunction<OwnProps, Props> {
   (ownProps: OwnProps): Props;
 }
 
-export function Konnekt<OwnProps, Props>(
-  hooksToProps: HooksToPropsFunction<OwnProps, Props>
+interface KonnektOptions {
+  getDisplayName: (name: string) => string;
+  displayName: string | undefined;
+}
+
+const defaultOptions: KonnektOptions = {
+  getDisplayName: (name: string) => `Konnekt(${name})`,
+  displayName: undefined,
+};
+
+function Konnekt<OwnProps, Props>(
+  hooksToProps: HooksToPropsFunction<OwnProps, Props>,
+  options: KonnektOptions = defaultOptions
 ) {
   return function(Component: React.ComponentType<OwnProps & Props>) {
-    return function(props: OwnProps) {
+    const Konnekted = function(props: OwnProps) {
       const hooksProps = hooksToProps(props);
       return <Component {...props} {...hooksProps} />;
     };
+    Konnekted.displayName =
+      options.displayName === undefined
+        ? options.getDisplayName(Component.displayName || Component.name)
+        : options.displayName;
+    return hoistStatics(Konnekted, Component);
   };
 }
+
+export default Konnekt;
